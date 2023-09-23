b4de03becaa7baefb53c479043c4ad2a761ad14f
unknown <jawadadriouach@gmail.com>
2023-09-23 01:04:54 +0100

    project de fin sas

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 #include <string.h>
 #include <time.h>
 struct Tache {
-    int id;
-    char titre[200];
-    char description[300];
-    char deadline[30];
-    char statu[20];
+    char id[100];
+    char titre[100];
+    char description[100];
+    char deadline[100];
+    char statu[100];
 
  };
 void ajouter(){
     struct Tache nevelltach;
     printf("entre une id:\n");
-    scanf("%d",nevelltach.id);
+    scanf("%s",nevelltach.id);
     printf("entre une titre:\n");
     scanf("%s",nevelltach.titre);
     printf("entre une description:\n");
@@ -25,40 +25,45 @@ void ajouter(){
 
 
 }
-void ajouterples(){
 
-    int i;
-     ajouter();
-    printf("Combien de taches souhaitez vous ajouter\n");
-    scanf("%d",i);
-    for(i=0;i<400;i++){
 
-    }
-    }
 int main() {
     struct Tache listeTaches[100];
     int choix;
+    int nombreTaches = 0;
     do{
     printf("~~~~~~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("1: Ajouter une nouvelle tache.\n");
-    printf("2: Ajouter plusieurs nouvelles taches.\n");
-    printf("3: Afficher la liste de toutes les taches.\n");
-    printf("4: Modifier une tache\n");
-    printf("5: Supprimer une tache par identifiant.\n");
-    printf("6: Rechercher les Taches.\n");
-    printf("7: Statistiques.\n");
+    printf("2: Afficher la liste de toutes les taches.\n");
+    printf("3: Modifier une tache\n");
+    printf("4: Supprimer une tache par identifiant.\n");
+    printf("5: Rechercher les Taches.\n");
+    printf("6: Statistiques.\n");
     printf("entre un choix:\n");
     scanf("%d",&choix);
 
     switch(choix){
 
     case 1:
-        printf("votre choix et 1\n");
-    ajouter();
+         printf("votre choix est 1\n");
+                if (nombreTaches < 100) {
+                    ajouter(&listeTaches[nombreTaches]);
+                    nombreTaches++;
+                } else {
+                    printf("La liste de taches est pleine.\n");
+                }
         break;
     case 2:
-        printf("votre choix et 2\n");
-ajouterples();
+        pprintf("votre choix est 2\n");
+                printf("Liste de toutes les taches :\n");
+                for (int i = 0; i < nombreTaches; i++) {
+                    printf("Tache %d :\n", i + 1);
+                    printf("ID : %s\n", listeTaches[i].id);
+                    printf("Titre : %s\n", listeTaches[i].titre);
+                    printf("Description : %s\n", listeTaches[i].description);
+                    printf("Date limite : %s\n", listeTaches[i].deadline);
+                    printf("Statu : %d\n", listeTaches[i].statu);
+
         break;
     case 3:
         printf("votre choix et 3\n");
